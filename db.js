b888f9ecda6b6ef4f4517dc38b175f694ce19c6f
vintagechocolate <vintagechocolate@gmx.us>
2024-04-28 18:24:51 -0400

    ...

--- db.js
+++ db.js
@@ -5,6 +5,6 @@ mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB'))
-.catch((err) => console.error('Error connecting to MongoDB:', err));
+.then(() => console.log('Connected to MongooseDB'))
+.catch((err) => console.error('Error connecting to MongooseDB:', err));
 
