1803b89bb1b646611e930ddd527bb824717acef9
vintagechocolate <vintagechocolate@gmx.us>
2024-04-28 18:24:02 -0400

    ...

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
+require("dotenv").config()
 const mongoose = require('mongoose');
 
-mongoose.connect(DB_URL, {
+mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((err) => console.error('Error connecting to MongoDB:', err));
+
